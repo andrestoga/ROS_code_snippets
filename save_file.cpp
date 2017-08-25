8a95db070eff68794b702b5b152ba8eddc8fcd0d
Andrés Torres García <atorresgarcia@ucmerced.edu>
2017-08-25 13:31:31 -0700

    Create save_file.cpp

---
+++ save_file.cpp
@@ -0,0 +1,8 @@
+std::ofstream file("/home/andrestoga/ros_Map_Merging/corner_data/breaking_points.txt", std::ofstream::out);
+
+for (int i = 0; i < vector.size(); ++i)
+{
+  file << i << " " << vector[ i ] << std::endl;
+}
+
+file.close();
