dfa8a22c1107af84f474cc7ec85616f5a610b11b
Andrés Torres García <atorresgarcia@ucmerced.edu>
2017-08-26 10:44:55 -0700

    Update Command_line_args.cpp

--- Command_line_args.cpp
+++ Command_line_args.cpp
@@ -1,4 +1,11 @@
 int main(int argc, char **argv)
 {
-
+  if( argc != 2 )
+	{
+		ROS_ERROR("Send as an argument the path where to save the file");
+		exit(-1);
+	}
+  
+  // The position 0 is the name of the program
+  std::string fname(argv[1]);
 }
